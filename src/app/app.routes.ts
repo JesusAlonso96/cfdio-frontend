413c75ad7efa6cebddd9295ba683d5fcaba8e232
JesusAlonso96 <56599871+JesusAlonso96@users.noreply.github.com>
2025-09-25 18:46:35 -0600

    wip: componentes basicos
    Se agregan componentes iniciales y Angular material para diseño

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+// import { authGuard } from './modules/auth/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./modules/auth/pages/login/login').then(m => m.LoginComponent),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./modules/auth/pages/register/register').then(m => m.RegisterComponent),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./modules/dashboard/pages/dashboard/dashboard').then(m => m.DashboardComponent),
+    //canActivate: [authGuard],
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
