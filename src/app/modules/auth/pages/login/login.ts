6cb3516aa7cb75e105dfe99a5e8db5e2c4e09d84
JesusAlonso96 <56599871+JesusAlonso96@users.noreply.github.com>
2025-09-28 17:28:24 -0600

    wip: pantalla login con material

--- src/app/modules/auth/pages/login/login.ts
+++ src/app/modules/auth/pages/login/login.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 @Component({
   selector: 'app-login',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule],
   standalone: true,
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class LoginComponent {
+  isDark = false;
+  toggleTheme() {
+    this.isDark = !this.isDark;
 
+
+    document.body.classList.toggle('dark-theme');
+
+  }
 }
