dc275ebd187fdc81e7d7a6b53b1fdd2e5f35d74b
JesusAlonso96 <56599871+JesusAlonso96@users.noreply.github.com>
2025-10-06 19:52:24 -0600

    wip: estilos iniciales de dashboard

--- src/app/modules/dashboard/pages/dashboard/dashboard.ts
+++ src/app/modules/dashboard/pages/dashboard/dashboard.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { Component, inject, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, MatCardModule],
   standalone: true,
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
 export class DashboardComponent {
 
+  // Datos de ejemplo vacíos - solo para estructura
+  navigationItems = ['Home', 'Manage', 'Master', 'Reports', 'Action Planning'];
+  activityUsers = Array(6).fill({ name: '', type: '', category: '' });
+  ongoingSurveys = Array(4).fill({ study: '', name: '' });
 }
