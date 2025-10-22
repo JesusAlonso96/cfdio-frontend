6a694727a2ab4e01d7215264e269cc4a49300bc3
JesusAlonso96 <jalonso.cl96@gmail.com>
2025-10-22 09:54:20 -0600

    feat: custom toast

---
+++ src/app/shared/components/toast/toast.ts
@@ -0,0 +1,54 @@
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, ChangeDetectorRef, Component, Input, OnInit, Signal, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { OutlinedIconDirective } from '../../directives/outlined-icon';
+import { ToastType } from '../../enums/toast-type.enum';
+
+@Component({
+  selector: 'app-toast',
+  imports: [CommonModule, MatIconModule, OutlinedIconDirective],
+  templateUrl: './toast.html',
+  styleUrl: './toast.scss'
+})
+export class ToastComponent implements AfterViewInit {
+  @Input() message = '';
+  @Input() type: ToastType = ToastType.Success;
+  visible = false;
+  toastIcon: string = '';
+
+  constructor(private cdr: ChangeDetectorRef) {
+
+  }
+
+  ngAfterViewInit() {
+    // Espera un ciclo para asegurar que el DOM pinte el estado inicial (invisible)
+    setTimeout(() => {
+      this.visible = true;
+      this.toastIcon = this.getCurrentIcon();
+      this.cdr.detectChanges();
+    }, 10);
+  }
+
+  async fadeOut(): Promise<void> {
+    this.visible = false;
+    this.cdr.detectChanges();
+
+    return new Promise(resolve => setTimeout(resolve, 300)); // Duración de la animación
+  }
+
+  close() {
+    this.fadeOut().then(() => this.onClose?.());
+  }
+
+  onClose?: () => void;
+
+  private getCurrentIcon(): string {
+    switch (this.type) {
+      case ToastType.Success: return 'check_circle_unread';
+      case ToastType.Error: return 'release_alert';
+      case ToastType.Warning: return 'warning';
+      case ToastType.Info: return 'info';
+      default: return 'check_circle_unread';
+    }
+  }
+}
---
+++ src/app/shared/services/toast.service.ts
@@ -0,0 +1,103 @@
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { ApplicationRef, ComponentRef, createComponent, inject, Injectable } from '@angular/core';
+import { ToastComponent } from '../components/toast/toast';
+import { ToastType } from '../enums/toast-type.enum';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+  private containerOverlayRef?: OverlayRef;
+  private containerElement?: HTMLElement;
+  private activeToasts: { componentRef: ComponentRef<ToastComponent> }[] = [];
+  private overlay = inject(Overlay);
+  private appRef = inject(ApplicationRef);
+  constructor() { }
+
+  show(message: string, type: ToastType, duration: number = 4000) {
+    this.ensureContainer();
+
+    // Crear el ToastComponent dinámicamente
+    const componentRef = createComponent(ToastComponent, {
+      environmentInjector: this.appRef.injector,
+    });
+    this.appRef.attachView(componentRef.hostView);
+    this.containerElement!.appendChild(componentRef.location.nativeElement);
+    componentRef.instance.message = message;
+    componentRef.instance.type = type;
+    componentRef.instance.onClose = () => this.removeToast(componentRef);
+
+    // Añadir al contenedor
+    this.containerElement!.appendChild(componentRef.location.nativeElement);
+
+    this.activeToasts.push({ componentRef });
+
+    this.repositionToasts();
+
+    // Cierre automático
+    setTimeout(() => componentRef.instance.close(), duration);
+  }
+
+  private ensureContainer() {
+    if (!this.containerOverlayRef) {
+      this.containerOverlayRef = this.overlay.create({
+        positionStrategy: this.overlay.position()
+          .global()
+          .top('20px')
+          .centerHorizontally(),
+        hasBackdrop: false,
+        scrollStrategy: this.overlay.scrollStrategies.noop(),
+      });
+
+      this.containerElement = this.containerOverlayRef.overlayElement;
+      this.containerElement.style.position = 'relative';
+      this.containerElement.style.display = 'flex';
+      this.containerElement.style.flexDirection = 'column';
+      this.containerElement.style.alignItems = 'center';
+      this.containerElement.style.pointerEvents = 'none';
+    }
+  }
+
+  private async removeToast(componentRef: ComponentRef<ToastComponent>) {
+    await componentRef.instance.fadeOut();
+
+    const index = this.activeToasts.findIndex(t => t.componentRef === componentRef);
+    if (index !== -1) this.activeToasts.splice(index, 1);
+
+    if (componentRef.location.nativeElement.parentNode) {
+      componentRef.location.nativeElement.parentNode.removeChild(componentRef.location.nativeElement);
+    }
+    this.appRef.detachView(componentRef.hostView);
+    componentRef.destroy();
+    this.repositionToasts();
+  }
+
+  private repositionToasts() {
+    const spacing = 10;
+    let currentTop = 0;
+
+    for (const toast of this.activeToasts) {
+      const el = toast.componentRef.location.nativeElement as HTMLElement;
+      el.style.top = `${currentTop}px`;
+      currentTop += el.offsetHeight + spacing;
+    }
+  }
+
+  /* Public methods */
+  public showSuccess(message: string, duration?: number) {
+    this.show(message, ToastType.Success, duration);
+  }
+
+  public showError(message: string, duration?: number) {
+    this.show(message, ToastType.Error, duration);
+  }
+
+  public showWarning(message: string, duration?: number) {
+    this.show(message, ToastType.Warning, duration);
+  }
+
+  public showInfo(message: string, duration?: number) {
+    this.show(message, ToastType.Info, duration);
+  }
+
+}
